905b5c00b3c211e6397107c16b77ccae9ac293ee
Aghamenin <aghamenin25@gmail.com>
2023-08-31 21:16:13 -0300

    trocando o nome do projeto

--- Conta-Banco/src/ContaTerminal.java
+++ Conta-Banco/src/ContaTerminal.java
@@ -1,7 +1,7 @@
 import java.util.Locale;
 import java.util.Scanner;
 
-public class BancoTerminal {
+public class ContaTerminal {
     public static void main(String[] args) throws Exception{
 
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
