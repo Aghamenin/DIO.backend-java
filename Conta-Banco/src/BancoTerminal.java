5a89e2b1a91f87a37e1757e1c5727ac1be0dd9be
Aghamenin <aghamenin25@gmail.com>
2023-08-31 21:12:27 -0300

    Adicionando um projeto da Dio

---
+++ Conta-Banco/src/BancoTerminal.java
@@ -0,0 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class BancoTerminal {
+    public static void main(String[] args) throws Exception{
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+         
+        System.out.println("Por favor digite seu número da conta: ");
+        int numero = scanner.nextInt();    
+
+        System.out.println("Digite a sua agência");
+        String agencia = scanner.next();
+
+        System.out.println("Digite seu nome: ");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Digite o valor do seu saldo: ");
+        double saldo = scanner.nextDouble();
+
+         System.out.println("Óla " + nomeCliente + ",obrigado por criar uma conta em nosso banco,\nsua agência é "  + agencia +
+",\nconta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+    }       
+}
