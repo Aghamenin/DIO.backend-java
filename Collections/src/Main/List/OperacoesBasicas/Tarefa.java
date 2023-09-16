99e68e6596fb4c6df331a2ac508d4242c3f52a98
Aghamenin Silva <aghamenin25@gmail.com>
2023-09-15 22:02:55 -0300

    Aula sobre List da DIO

---
+++ Collections/src/Main/List/OperacoesBasicas/Tarefa.java
@@ -0,0 +1,21 @@
+package Main.List.OperacoesBasicas;
+
+public class Tarefa {
+    //Atributos
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString() {
+        return descricao;
+    }
+
+    
+}
